c7d23d77a365b35efe12bc00c458e6a1f91a8e19
ozikelvin <isaackelvin378@gmail.com>
2022-07-17 00:11:55 +0100

    'quick-fix'

--- src/components/Table/row.js
+++ src/components/Table/row.js
@@ -7,13 +7,7 @@ const Row = ({ item , setValue , setShow }) =>
 {
     const style = { margin: '1em auto', padding: '0.2em 1em', borderRadius: '0.2em', };
     const arrKey = Object.keys(item);
-    // return (item.map((val, id) => id == 5 ? <Button key={val} label='view result' style={style} /> : <TD key={val}>{val}</TD>))
-    // return item.map((val, id) => <TD key={val}>
-    //     {id == 5 ? <>
-    //         <Button key={val} onClick={()=> {setValue(item); setShow(true) }} label='view result' style={style} />
-    //         <Menu />
-    //     </> : val}
-    //     </TD>)
+
         return (
             <>
                 {
